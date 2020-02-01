a866aa5505f3033493fc7780a322d096c682d8ee
Avalon <littlecontrol2019@gmail.com>
2020-02-01 09:51:26 +0800

    [无重复字符的最长字串] [字符匹配] [滑动窗口]

---
+++ leetcode/003_无重复字符的最长字串/test.js
@@ -0,0 +1,21 @@
+let str = 'NothingToDo'
+let arr = str.split('')
+// console.log(arr)
+
+let arr2 = []
+arr2[0] = new Array()
+arr2[0][0] = arr[0]
+// console.log(arr2)
+
+arr3 = new Array(arr[0])
+// console.log(arr3)
+
+/* for (const item of str) {
+    console.log(item)
+    console.log(str.indexOf(item))
+    console.log('============')
+}
+ */
+
+console.log(arr.includes('N'))
+
