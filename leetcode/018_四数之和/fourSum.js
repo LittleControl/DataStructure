85ca1f18d43620745ba2d7a70a4b05b0d576cc75
LittleControl <littlecontrol2019@gmail.com>
2020-04-11 20:22:41 +0800

    四数想加(双指针)

--- leetcode/018_四数之和/fourSum.js
+++ leetcode/018_四数之和/fourSum.js
@@ -21,42 +21,29 @@
  */
 var fourSum = function (nums, target) {
     let res = []
-    if (nums.length < 4) {
-        return res
-    }
+    if (nums.length < 4) return res
     nums.sort((a, b) => a - b)
     for (let i = 0; i < nums.length - 3; i++) {
-        if (nums[i] > target) {
-            return res
-        }
-        if (i > 0 && nums[i] == nums[i - 1]) {
-            continue
-        }
+        if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break
+        if (nums[i] + nums[nums.length - 1] + nums[nums.length - 2] + nums[nums.length - 3] < target) continue
+        if (i > 0 && nums[i] == nums[i - 1]) continue
         for (let j = i + 1; j < nums.length - 2; j++) {
-            if (j > i + 2 && nums[j] == nums[j - 1]) {
-                continue
-            }
+            if (j > i + 1 && nums[j] == nums[j - 1]) continue
+            if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) continue
+            if (nums[i] + nums[j] + nums[nums.length - 1] + nums[nums.length - 2] < target) continue
             let k = j + 1
             let l = nums.length - 1
-            let sum = nums[j] + nums[k] + nums[l] + nums[i]
             while (k < l) {
+                let sum = nums[i] + nums[j] + nums[k] + nums[l]
                 if (sum == target) {
                     res.push([nums[i], nums[j], nums[k], nums[l]])
-                    while (k < l && nums[k] == nums[k + 1]) {
-                        k--
-                    }
-                    while (k < l && nums[l] == nums[l - 1]) {
-                        l--
-                    }
+                    while (k < l && nums[k] == nums[k + 1]) k++
+                    while (k < l && nums[l] == nums[l - 1]) l--
                     k++
                     l--
                 }
-                if (sum > 0) {
-                    l--
-                }
-                if (sum < 0) {
-                    k++
-                }
+                if (sum < target) k++
+                if (sum > target) l--
             }
         }
     }
@@ -64,5 +51,11 @@ var fourSum = function (nums, target) {
 }
 
 let nums = [1, 0, -1, 0, -2, 2]
+// nums = [0, 0, 0, 0]
 let target = 0
+// target = 1
+// nums = [-3, -1, 0, 2, 4, 5]
+// target = 0
+nums = [-3, -2, -1, 0, 0, 1, 2, 3]
+target = 0
 console.log(fourSum(nums, target))
