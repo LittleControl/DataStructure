fdfa5321c9c8df80f06dba54048af70e8758165f
LittleControl <litlecontrol2019@gmail.com>
2020-11-28 15:20:00 +0800

    例题解析

--- LeetCode/036_有效的数独/isValidSudoku.js
+++ LeetCode/036_有效的数独/isValidSudoku.js
@@ -95,3 +95,27 @@ arr = [
 ]
 
 console.log(isValidSudoku(arr))
+
+
+let solution = (arr) => {
+    let rows = {}
+    let columns = {}
+    let boxes = {}
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            let num = arr[i][j]
+            if (num !== '.') {
+                let boxIndex = parseInt(i / 3) + parseInt(j / 3) * 3
+                if (rows[i + '-' + num] || columns[j + '-' + num] || boxes[boxIndex + '-' + num]) {
+                    return false
+                }
+                rows[i + '-' + num] = true
+                columns[j + '-' + num] = true
+                boxes[boxIndex + '-' + num] = true
+            }
+        }
+    }
+    return true
+}
+
+console.log(solution(arr))
