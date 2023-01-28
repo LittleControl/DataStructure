e4cc008e3d871ed8c26661ca77b3a5c569a8c0b7
littlecontrol <i@littlecontrol.me>
2023-01-28 21:58:34 +0800

    feat: db_176 第二高的薪水

--- LeetCode_db/176.sql
+++ LeetCode_db/176.sql
@@ -1,22 +1,37 @@
+CREATE TABLE
+  Employee (
+    `id` INT(11) NOT NULL,
+    `salary` DOUBLE NOT NULL,
+    PRIMARY KEY (`id`)
+  )
+;
+
+INSERT INTO
+  Employee (id, salary)
+VALUES
+  (1, 100),
+  (2, 200),
+  (3, 300)
+;
+
 SELECT
   CASE
-    WHEN COUNT(1) = 1 THEN NULL
-    ELSE MIN(t1.salary)
-  END
+    WHEN (COUNT(1) = 1) THEN t1.salary
+    ELSE NULL
+  END AS 'SecondHighestSalary'
 FROM
   (
     SELECT
-      id,
-      salary
+      e1.salary,
+      COUNT(DISTINCT e2.salary) AS 'rank'
     FROM
-      Employee e1
+      Employee e1,
+      Employee e2
+    WHERE
+      e1.salary <= e2.salary
     GROUP BY
-      id
-    ORDER BY
-      salary DESC
-    LIMIT
-      2
+      e1.salary
   ) t1
-GROUP BY
-  t1.id
+WHERE
+  t1.rank = '2'
 ;
