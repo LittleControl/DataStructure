42b2e1ce317237f304ad166f53c7311b456d40bd
LittleControl <i@littlecontrol.cc>
2023-03-15 00:00:35 +0800

    feat: other solution

--- LeetCode_db/184.sql
+++ LeetCode_db/184.sql
@@ -15,3 +15,30 @@ JOIN      (
 AND       e.departmentId = t.departmentId
           )
 ;
+
+-- 两个in值连用
+SELECT    d.name   AS 'Department',
+          e.name   AS 'Employee',
+          e.salary AS 'Salary'
+FROM      Employee e
+LEFT JOIN Department d ON e.departmentId = d.id
+WHERE     (e.departmentId, e.salary) IN (
+          SELECT    e2.departmentId,
+                    MAX(e2.salary)
+          FROM      Employee e2
+          GROUP BY  e2.departmentId
+          )
+;
+
+-- 使用all
+SELECT    d.name   AS 'Department',
+          e.name   AS 'Employee',
+          e.salary AS 'Salary'
+FROM      Employee e
+LEFT JOIN Department d ON e.departmentId = d.id
+WHERE     e.salary >= ALL (
+          SELECT    e2.salary
+          FROM      Employee e2
+          WHERE     e2.departmentId = e.departmentId
+          )
+;
