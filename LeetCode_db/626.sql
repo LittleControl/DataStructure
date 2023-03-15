4f437185478e2a7b9d51174486ebe47276ccb2e9
littlecontrol <i@littlecontrol.me>
2023-03-16 01:11:56 +0800

    feat: 位运算

--- LeetCode_db/626.sql
+++ LeetCode_db/626.sql
@@ -1,18 +1,17 @@
 -- Seat
 -- id student
-SELECT    t2.rn_new  AS 'id',
+SELECT    t2.rn_new  AS `id`,
           t2.student
 FROM      (
           SELECT    id,
                     student,
                     CASE
-                              WHEN rn % 2 = 1
-                              AND       rn = (
-                                        SELECT    COUNT(1)
-                                        FROM      Seat
-                                        ) THEN rn
-                                        WHEN rn % 2 = 1 THEN rn + 1
-                                        ELSE rn - 1
+                              WHEN rn = (
+                              SELECT    COUNT(1)
+                              FROM      Seat
+                              ) THEN rn
+                              WHEN rn % 2 = 1 THEN rn + 1
+                              ELSE rn - 1
                     END AS 'rn_new'
           FROM      (
                     SELECT    id,
@@ -21,7 +20,34 @@ FROM      (
                               ORDER BY  id
                               ) AS 'rn'
                     FROM      Seat
-                    ) t1
-          ) t2
+                    ) AS `t1`
+          ) AS `t2`
 ORDER BY  t2.rn_new
+;
+
+-- case优化
+SELECT    (
+          CASE
+                    WHEN MOD (id, 2) != 0
+                    AND       counts != id THEN id + 1
+                              WHEN MOD (id, 2) != 0
+                    AND       counts = id THEN id
+                              ELSE id - 1
+          END
+          ) AS id,
+          student
+FROM      seat,
+          (
+          SELECT    COUNT(*) AS counts
+          FROM      seat
+          ) AS seat_counts
+ORDER BY  id ASC
+;
+
+--位运算
+SELECT    s1.id,
+          COALESCE(s2.student, s1.student) AS student
+FROM      seat s1
+LEFT JOIN seat s2 ON ((s1.id + 1) ^ 1) - 1 = s2.id
+ORDER BY  s1.id
 ;
