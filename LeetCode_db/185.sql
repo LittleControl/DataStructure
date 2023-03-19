7d361d116006159a912c94b4183fbd79e7e2f91a
littlecontrol <i@littlecontrol.me>
2023-03-20 00:11:18 +0800

    feat: 官方无窗口函数解法

--- LeetCode_db/185.sql
+++ LeetCode_db/185.sql
@@ -9,11 +9,25 @@ FROM      (
                     e.departmentId,
                     d.name            AS 'Department',
                     DENSE_RANK() OVER (
-                    PARTITION BY departmentId
-                    ORDER BY  salary DESC
+                    PARTITION BY e.departmentId
+                    ORDER BY  e.salary DESC
                     ) AS 'rn'
-          FROM      Employee e
-          JOIN      Department d ON e.departmentId = d.id
+          FROM      Employee AS `e`
+          JOIN      Department AS `d` ON e.departmentId = d.id
           ) AS `t`
 WHERE     t.rn <= 3
+;
+
+-- 官方solution join和子查询
+SELECT    d.Name    AS 'Department',
+          e1.Name   AS 'Employee',
+          e1.Salary
+FROM      Employee e1
+JOIN      Department d ON e1.DepartmentId = d.Id
+WHERE     3 > (
+          SELECT    COUNT(DISTINCT e2.Salary)
+          FROM      Employee e2
+          WHERE     e2.Salary > e1.Salary
+          AND       e1.DepartmentId = e2.DepartmentId
+          )
 ;
