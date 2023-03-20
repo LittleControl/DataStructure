ac18c35ad3b425e9237be4120a5430b0986e5ac4
LittleControl <i@littlecontrol.cc>
2023-03-20 23:51:25 +0800

    feat: 连续join

--- LeetCode_db/262.sql
+++ LeetCode_db/262.sql
@@ -102,3 +102,16 @@ AND       r.driver IS NOT NULL
 GROUP BY  r.request_at
 ORDER BY  r.request_at
 ;
+
+-- 别人解法,连续两次join,我是傻逼
+SELECT    t.request_at                                          DAY,
+          ROUND(AVG (t.status <> 'completed'), 2) 'Cancellation Rate'
+FROM      Trips t
+INNER     JOIN Users u1 ON t.client_id = u1.users_id
+AND       u1.banned = 'No'
+INNER     JOIN Users u2 ON t.driver_id = u2.users_id
+AND       u2.banned = 'No'
+WHERE     t.request_at >= "2013-10-01"
+AND       t.request_at <= "2013-10-03"
+GROUP BY  t.request_at
+;
