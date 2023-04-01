38986fb17bf430242a74bf94bb281e7b74733a45
littlecontrol <i@littlecontrol.me>
2023-04-01 17:56:24 +0800

    feat: 官方解法+窗口函数解法

--- LeetCode_db/585.sql
+++ LeetCode_db/585.sql
@@ -7,6 +7,7 @@
 -- | LAT         | NUMERIC(5,2)  |
 -- | LON         | NUMERIC(5,2)  |
 --
+-- res0
 SELECT    ROUND(SUM(r1.TIV_2016), 2) AS 'tiv_2016'
 FROM      (
           SELECT    DISTINCT    i1.pid,
@@ -24,4 +25,39 @@ FROM      (
                     )
           AND       i1.TIV_2015 = i2.TIV_2015
           ) r1
+;
+
+-- 官方解法
+SELECT    SUM(tiv_2016)
+FROM      insurance
+WHERE     TIV_2015 IN (
+          SELECT    TIV_2015
+          FROM      insurance
+          GROUP BY  TIV_2015
+          HAVING    (COUNT(1) > 1)
+          )
+AND       pid IN (
+          SELECT    pid
+          FROM      insurance
+          GROUP BY  lon,
+                    LAT
+          HAVING    (COUNT(*) = 1)
+          )
+;
+
+-- 窗口函数
+SELECT    SUM(tiv_2016)
+FROM      (
+          SELECT    *,
+                    COUNT(pid) OVER (
+                    PARTITION BY TIV_2015
+                    ) AS 'cnt1',
+                    COUNT(pid) OVER (
+                    PARTITION BY lon,
+                              LAT
+                    ) AS 'cnt2'
+          FROM      insurance
+          ) t1
+WHERE     cnt1 > 1
+AND       cnt2 = 1
 ;
